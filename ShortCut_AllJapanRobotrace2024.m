82376e3758e2fd7cd9cece357fd99828b1c13f47
Juing-Huei Su <67456710+suhulhu@users.noreply.github.com>
2025-07-02 12:47:10 +0800

    Add files via upload

--- ShortCut_AllJapanRobotrace2024.m
+++ ShortCut_AllJapanRobotrace2024.m
@@ -4,22 +4,11 @@ dSize = max(size(xyCourse));
 fxyCourse = zeros(dSize, 2);
 fxyCourse(1,:) = xyCourse(1,:);
 
-a = 0.95;
 for i = 2:dSize
-    fxyCourse(i,1) = a*fxyCourse(i-1,1) + (1-a)*xyCourse(i,1);
-    fxyCourse(i,2) = a*fxyCourse(i-1,2) + (1-a)*xyCourse(i,2);
+    fxyCourse(i,1) = 0.95*fxyCourse(i-1,1) + 0.05*xyCourse(i,1);
+    fxyCourse(i,2) = 0.95*fxyCourse(i-1,2) + 0.05*xyCourse(i,2);
 end;
 
 f2 = figure(2);
-set(f2, 'color', 'white');
-plot(fxyCourse(:,1), fxyCourse(:,2),'b', xyCourse(:,1), xyCourse(:,2),'r--', 'LineWidth',2);grid;
-legend('Shortcut course','Original course', 'FontSize',14, 'Location', 'southeast');
-% hold;
-% plot(xyCourse(:,1), xyCourse(:,2),'r', 'LineWidth',1);hold
-axis([100 400 0 100]); 
-% axis([0 955 0 370]); 
-set(gca,'DataAspectRatio',[1 1 1]);
-set(gca,'FontSize',14);
-title('2024 All Japan Robotrace contest course - Shortcut test', 'fontsize',18);
-xlabel('Length in cm', 'fontsize',16);
-ylabel('Length in cm', 'fontsize',16);
+plot(fxyCourse(:,1), fxyCourse(:,2),'k.', 'LineWidth',3);hold
+plot(xyCourse(:,1), xyCourse(:,2),'r', 'LineWidth',3);
